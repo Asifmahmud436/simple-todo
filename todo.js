536ac214201b21f81e668c2d439406d502162bbb
Zettex <safaandsafa4@gmail.com>
2025-01-21 17:18:16 +0600

    added the delete functionality

--- todo.js
+++ todo.js
@@ -1,19 +1,25 @@
 const container = document.querySelector('.root')
+let id = 0
 
 function makeTodo(event){
     event.preventDefault()
     const text = document.querySelector('#todo').value;
     container.innerHTML +=`
-        <div>
+        <div id=${id}>
             <p>${text}</p>
-            <button onclick="deleteTodo()" type="button">Delete</button>
-            <button onclick="editTodo()" type="button">Edit</button>
+            <button onclick="deleteTodo(${id})" type="button">Delete</button>
+            <button onclick="editTodo(${id})" type="button">Edit</button>
         </div>
     `
-
-    document.querySelector('#todo').value = ' '
+    id++;
+    document.querySelector('#todo').value = ' ';
 }
 
-function deleteTodo(){
-    
+function deleteTodo(id){
+    const allChildren = container.children
+    for(child of allChildren){
+        if(child.id == id){
+            child.remove()
+        }
+    }
 }
