e981c6d5f02b4a6be10a8d983bff9d35bb8eeffc
Zettex <safaandsafa4@gmail.com>
2025-01-21 18:32:00 +0600

    implemented the edit functionality

--- todo.js
+++ todo.js
@@ -7,11 +7,13 @@ function makeTodo(event){
     container.innerHTML +=`
         <div id=${id}>
             <p>${text}</p>
-            
-            <form onsubmit="editForm(event)" style="display: none;">
-                <input type="text" name="editTodo" id="editTodo" placeholder="edit the todo">
+
+            <form onsubmit="editForm(${id},event)" style="display: none;">
+                <input type="text" name="newTodo" id=${id} placeholder="edit the todo">
                 <input type="submit" value="Save">
             </form>
+
+
             <button onclick="deleteTodo(${id})" type="button">Delete</button>
             <button onclick="editTodo(${id})" type="button">Edit</button>
         </div>
@@ -33,7 +35,18 @@ function editTodo(id){
     const allChildren = container.children
     for(child of allChildren){
         if(child.id == id){
-            console.log(child.children[2].style.display = 'block')
+            child.children[1].style.display = 'block'
+        }
+    }
+}
+
+function editForm(id,event){
+    event.preventDefault()
+    const allChildren = container.children
+    for(child of allChildren){
+        if(child.id == id){
+            child.children[0].innerText = child.children[1][0].value
+            child.children[1].style.display = 'none'
         }
     }
 }
