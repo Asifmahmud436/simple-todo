6e1b9653fa198fb0561bed0cfd295db79f269510
Zettex <safaandsafa4@gmail.com>
2025-01-21 18:48:55 +0600

    made the id quite unique

--- todo.js
+++ todo.js
@@ -1,8 +1,17 @@
 const container = document.querySelector('.root')
-let id = 0
+
+function generateEpicId(){
+    let arbitaryArray = `0123456789qwertyuioplkjhagsdfmznxbcv`
+    let beatifulId = ''
+    for(let i = 0; i < 13;i++){
+        beatifulId += arbitaryArray[Math.floor(Math.random()*26)]
+    }
+    return 'a' + beatifulId
+}
 
 function makeTodo(event){
     event.preventDefault()
+    let id = generateEpicId()
     const text = document.querySelector('#todo').value;
     container.innerHTML +=`
         <div id=${id}>
@@ -13,38 +22,37 @@ function makeTodo(event){
                 <input type="submit" value="Save">
             </form>
 
-
             <button onclick="deleteTodo(${id})" type="button">Delete</button>
             <button onclick="editTodo(${id})" type="button">Edit</button>
         </div>
     `
-    id++;
     document.querySelector('#todo').value = ' ';
 }
 
-function deleteTodo(id){
+function deleteTodo(element){
     const allChildren = container.children
     for(child of allChildren){
-        if(child.id == id){
+        if(child.id == element.id){
             child.remove()
+            
         }
     }
 }
 
-function editTodo(id){
+function editTodo(element){
     const allChildren = container.children
     for(child of allChildren){
-        if(child.id == id){
+        if(child.id == element.id){
             child.children[1].style.display = 'block'
         }
     }
 }
 
-function editForm(id,event){
+function editForm(element,event){
     event.preventDefault()
     const allChildren = container.children
     for(child of allChildren){
-        if(child.id == id){
+        if(child.id == element.id){
             child.children[0].innerText = child.children[1][0].value
             child.children[1].style.display = 'none'
         }
